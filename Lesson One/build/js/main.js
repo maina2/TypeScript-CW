e4694aebb0ded314f5fac91d04d855010952d224
Simon Chanzu <chanzusimon6@gmail.com>
2025-01-25 11:18:09 +0300

    uploads today

--- Lesson One/build/js/main.js
+++ Lesson One/build/js/main.js
@@ -1,3 +1,7 @@
 "use strict";
-let username = "You";
+let username = "Them";
 console.log(username);
+let a = 12;
+let b = '3';
+let c = 2;
+console.log(a / b);
--- Lesson One/src/main.ts
+++ Lesson One/src/main.ts
@@ -1,3 +1,9 @@
-let username = "You"
+let username = "Them"
 
 console.log(username)
+
+let a = 12
+let b = '4'
+let c = 2
+
+console.log(a / b)
